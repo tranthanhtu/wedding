7b17435ea305cdd6c433e33ac0e224ac58a1a059
Tran Thanh Tu <tranthanhtuxd2211@gmail.com>
2016-06-06 16:13:31 +0700

    wedding

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/<path:path>")
+def static_file(path):
+    return app.send_static_file(path)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/forget')
+def forget():
+    return render_template("forget.html")
+
+@app.route('/happyending')
+def happyending():
+    return render_template("index.html")
+
+@app.route('/love')
+def love():
+    return render_template("love.html")
+
+
+
+if __name__ == '__main__':
+    app.run()
